6cf385d8548a9925fee48ab186b8bddff885294e
4ra1n <2023503307@qq.com>
2024-01-05 18:56:16 +0800

    docker file build

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,12 @@
 IMAGE_NAME="y4-lang"
 CONTAINER_NAME="y4-lang-builder"
 
+EXISTING_IMAGE=$(docker images -q $IMAGE_NAME:latest)
+if [ ! -z "$EXISTING_IMAGE" ]; then
+    echo "image $IMAGE_NAME:latest already exists. deleting..."
+    docker rmi $EXISTING_IMAGE
+fi
+
 docker build -t $IMAGE_NAME .
 CONTAINER_ID=$(docker run -d --name $CONTAINER_NAME $IMAGE_NAME)
 echo "container is running with ID: $CONTAINER_ID"
